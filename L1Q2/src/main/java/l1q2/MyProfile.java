d1d67ca9eb20349b13cebb027479d7b7ac242776
Tneoh <107362144+chuanlintneoh@users.noreply.github.com>
2023-02-05 15:27:25 +0800

    Add files via upload

---
+++ L1Q2/src/main/java/l1q2/MyProfile.java
@@ -0,0 +1,18 @@
+package l1q2;
+
+/**
+ *
+ * @author chuan
+ */
+public class MyProfile {
+       public static void main(String[] args) {
+        System.out.println("Name: Tneoh Chuan Lin");
+        System.out.println("Matric Number: 22004808");
+        System.out.println("Address: ");
+        System.out.println("F306, Kolej Kediaman Kinabalu, ");
+        System.out.println("Universiti Malaya, ");
+        System.out.println("50603 Kuala Lumpur, Wilayah Persekutuan Kuala Lumpur. ");
+        System.out.println("Email: 22004808@siswa.um.edu.my");
+        System.out.println("Contact No.: 018-9482119");
+    }
+}
---
+++ L1Q3/src/main/java/com/mycompany/l1q3/figure.java
@@ -0,0 +1,20 @@
+package com.mycompany.l1q3;
+
+/**
+ *
+ * @author chuan
+ */
+public class figure {
+    public static void main(String[] args) {
+        System.out.println("*********\t   ***   \t  *  \t    *");
+        System.out.println("*       *\t *     * \t *** \t   * *");
+        System.out.println("*       *\t*       *\t*****\t  *   *");
+        System.out.println("*       *\t*       *\t  *  \t *     *");
+        System.out.println("*       *\t*       *\t  *  \t*       *");
+        System.out.println("*       *\t*       *\t  *  \t *     *");
+        System.out.println("*       *\t*       *\t  *  \t  *   *");
+        System.out.println("*       *\t *     * \t  *  \t   * *");
+        System.out.println("*********\t   ***   \t  *  \t    *");
+    }
+    
+}
